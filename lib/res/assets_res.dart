c1e3b9e7184325c90dffb77bff040a41fd8b0d3a
Fakhry H. Al-Naffar <fakhry.h.alnaffar@gmail.com>
2023-09-22 20:50:47 +0300

    update version

--- lib/res/assets_res.dart
+++ lib/res/assets_res.dart
@@ -4,5 +4,5 @@
 // ignore_for_file: lines_longer_than_80_chars
 class AssetsRes {
   static const String PLUGIN_NAME = 'awesome_dialog';
-  static const String PLUGIN_VERSION = '3.1.0';
+  static const String PLUGIN_VERSION = '3.2.0';
 }
